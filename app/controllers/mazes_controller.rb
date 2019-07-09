abf8f4f4edbb73b452caa17ea0a0081467f5b7f7
Matt Williams <matt.seongjun@gmail.com>
2019-07-09 11:42:52 -0400

    Increase maze dimensions

--- app/controllers/mazes_controller.rb
+++ app/controllers/mazes_controller.rb
@@ -1,6 +1,6 @@
 class MazesController < ApplicationController
     def index
-        maze = Maze.new(x_dimension: 15, y_dimension: 15)
+        maze = Maze.new(x_dimension: 15, y_dimension: 25)
         render json: maze
     end
 end
--- app/models/maze.rb
+++ app/models/maze.rb
@@ -2,8 +2,6 @@ class Maze < ApplicationRecord
     after_initialize :gen_raw_maze
 
     def gen_raw_maze
-        x = self.x_dimension
-        y = self.y_dimension
-        self.raw_maze = MazeGen.new(x, y).print
+        self.raw_maze = MazeGen.new(self.x_dimension, self.y_dimension).print
     end
 end
