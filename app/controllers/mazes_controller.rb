6544efc8f39cbcf90c7278f2eb009be3bb321731
Matt Williams <matt.seongjun@gmail.com>
2019-07-10 13:08:41 -0400

    Adjust default dimensions

--- app/controllers/mazes_controller.rb
+++ app/controllers/mazes_controller.rb
@@ -2,7 +2,7 @@
 
 class MazesController < ApplicationController
   def index
-    maze = Maze.new(x_dimension: 45, y_dimension: 45)
+    maze = Maze.new(x_dimension: 40, y_dimension: 40)
     render json: maze
   end
 end
